dd8d113271c2785a6e7ac73f45772177318f75b7
OctDeKos <ymdyadin@gmail.com>
2022-11-11 11:51:12 +0300

    1 lesson. xpath.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,26 @@
+//подключаем библиотеки selenium
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+//подключение библиотеки для использования ожидания
+import java.time.Duration;
 
 public class Main {
     public static void main(String[] args) {
+        //прописываем расположение драйвера на компьютере
         System.setProperty("webdriver.chrome.driver","C:\\tools\\chromedriver\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-        driver.get("https://google.com");
+        //Установка ожидания
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://avito.ru");
+        //используем xpath для поиска нужно элемента по тексту, выбираем первый из найденных.
+        WebElement input = driver.findElement(By.xpath("(//a[text()='Личные вещи'])[1]"));
 
-        WebElement input = driver.findElement(By.xpath("//input[@aria-label='Найти']"));
-        WebElement inputs = driver.findElements(By.xpath("//input[@aria-label='Найти']"));
+//          WebElement input = driver.findElement(By.xpath("//input[@aria-label='Найти']"));
+//        WebElement inputs = driver.findElements(By.xpath("//input[@aria-label='Найти']"));
+
+        //кликаем по найденному элементу
         input.click();
     }
 }
